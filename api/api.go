2f6041a520a189c2ca5f8567fae4d3ee11eff7b9
guilhermevicente <guilhermedovicente@gmail.com>
2025-09-27 11:16:39 -0400

    refactor handler codes to separate in another file

--- api/api.go
+++ api/api.go
@@ -1,10 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/google/uuid"
 	"github.com/guilhermevicente/person-management/db"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -44,41 +40,4 @@ func (api *API) ConfigRoutes() {
 	api.Echo.GET("/persons/:id", api.getPerson)
 	api.Echo.PUT("/persons/:id", api.updatePerson)
 	api.Echo.DELETE("/persons/:id", api.deletePerson)
-}
-
-// Handler
-func (api *API) getPersons(c echo.Context) error {
-	persons, err := api.DB.GetPersons()
-	if err != nil {
-		return c.String(http.StatusNoContent, "Don't have person")
-	}
-	return c.JSON(http.StatusOK, persons)
-}
-
-func (api *API) createPerson(c echo.Context) error {
-	person := db.Person{}
-	if err := c.Bind(&person); err != nil {
-		return err
-	}
-	person.Id = uuid.New()
-	person.Deleted = false
-	if err := api.DB.InsertPerson(person); err != nil {
-		return c.String(http.StatusInternalServerError, "Error to create person")
-	}
-	return c.String(http.StatusOK, "Person created")
-}
-
-func (api *API) getPerson(c echo.Context) error {
-	personId := c.Param("id")
-	return c.String(http.StatusOK, fmt.Sprintf("Get person of id %s", personId))
-}
-
-func (api *API) updatePerson(c echo.Context) error {
-	personId := c.Param("id")
-	return c.String(http.StatusOK, fmt.Sprintf("Update person of id %s", personId))
-}
-
-func (api *API) deletePerson(c echo.Context) error {
-	personId := c.Param("id")
-	return c.String(http.StatusOK, fmt.Sprintf("Delete person of id %s", personId))
 }
